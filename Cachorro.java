b06151dc9236a386f0c7f4ef1e2b323909f33ad4
ivam <ivamwenderqueiroz@gmail.com>
2025-11-06 14:52:27 -0300

    Exclusão de objeto cachorro

--- Cachorro.java
+++ Cachorro.java
@@ -1,35 +1,60 @@
-public class Cachorro{
-
+public class Cachorro {
+    
     //atributos
     public String nome;
     public String raca;
     public int idade;
+
     
-    /*Construtor = método que inicializa objetos desta classe.
-     * Defiinição: método especial que inicializa objetos desta classe
-     * Sempre tem o mesmo nome da classe
-     * Não tem retorno
-     * Pode-se ter mais de um construtor na mesma classe
+    /*Construtor 
+        - definição:  método especial que incializa objetos desta classe.
+        - caracteristicas: 
+            - sempre tem o mesmo nome da classe
+            - não tem retorno 
+            - pode-se ter mais de um construtor na mesma classe
+        */
+        public Cachorro(String n, String raca, int idade) {
+            this.nome = n;
+            this.raca = raca;
+            this.idade = idade;
+        }
+    
+        public Cachorro(String n, String raca) {
+            this.nome = n;
+            this.raca = raca;
+        } 
+    
+        public Cachorro() {
+    
+        }
+
+
+
+    /*metodo latir que não retorna nada:
+        public void latir(){...}
+            public -> indica que esse método é visível para todos
+            void -> indica que o método tem um retorno vazio
+            latir -> nome que escolhemos para o métodos
+            () -> dentro do parenteses vazio, mostra que o método não espera receber nenhum valor 
     */
-    
 
-    public Cachorro(String n, String r, int i){
-        this.nome = n;
-        this.raca = r;
-        this.idade = i;
-    }
-
-    //método latir que não retorna nada:
-    
     public void latir(){
-        System.out.println(x:"Au au");
+        System.out.println("Au au");
     }
+
+    //método latir que retorna uma string 
     public String latir2(){
-        return "Au au";
+
+        String str = "Au au de latir2";
+        return str;
     }
 
     public void imprimir(){
 
-    System.out.println("nome:"+nome +"raça: "+raca +" idade: "+idade);
+        
+        System.out.println("nome: "+nome +"  raça: "+ raca +" idade: " + idade);
     }
+    
+
+
 }
--- Main.java
+++ Main.java
@@ -1,12 +1,30 @@
 public class Main {
-    public static void main(String[]args) throws Exception{
+    public static void main(String[] args) throws Exception {
+        
+        Cachorro c1 = new Cachorro("c1:  bob", "lavrador", 4); //criando um objeto cachorro
+        Cachorro c2 = new Cachorro("c2: charlie", "pitbull", 7);
+        Cachorro c3 = new Cachorro("c3:  nick", "pinch", 2);
 
-        Cachorro c1 = new Cachorro(n:"bob","lavrador"4); //criando objeto cachorro
-        Cachorro c2 =  new Cachorro(n:"lucas","viralata"7);
-        Cachorro c3 = new Cachorro("pedro","chiuaua"9);
+        Cachorro c4 = new Cachorro("c4:  nick", "pinch");
+
+        Cachorro c5 = new Cachorro();
+
+ 
+
+        c5.nome  = "joao";
+        c5.idade = 7;
+        c5.raca = "lavrador";
+        
 
         c1.imprimir();
         c2.imprimir();
         c3.imprimir();
+        c4.imprimir();
+        c5.imprimir();
+
+
+        String retornoDoMetodoLatir2 =  c1.latir2();
+
+        System.out.println(retornoDoMetodoLatir2);
     }
 }
